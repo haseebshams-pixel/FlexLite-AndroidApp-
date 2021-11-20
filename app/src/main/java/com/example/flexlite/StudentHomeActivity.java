e55fd199b67f6d88ef1155dd878b284424ba7234
haseebshams-pixel <hadishams38.hs@gmail.com>
2021-11-21 02:39:56 +0500

    student navigation drawer added

--- app/src/main/java/com/example/flexlite/StudentHomeActivity.java
+++ app/src/main/java/com/example/flexlite/StudentHomeActivity.java
@@ -1,14 +1,79 @@
 package com.example.flexlite;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class StudentHomeActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class StudentHomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    ImageView menuIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_home);
+
+
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.stud_drawer);
+        navigationView = (NavigationView) findViewById(R.id.stud_nav);
+        menuIcon = (ImageView) findViewById(R.id.stud_menu_icon);
+
+
+        navigationDrawer();
+
+
+
+
+    }
+
+    private void navigationDrawer() {
+        navigationView.bringToFront();
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_home);
+
+        menuIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (drawerLayout.isDrawerVisible(GravityCompat.START)) {
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                } else {
+                    drawerLayout.openDrawer(GravityCompat.START);
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerVisible(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_attendance:
+                Toast.makeText(this, "Attendance", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
     }
 }
